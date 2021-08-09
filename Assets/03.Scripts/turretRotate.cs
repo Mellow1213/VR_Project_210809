e9a5f09cc01ac310aea31ee9eccf8748409ac529
Mellow1213 <dpcks7282@gmail.com>
2021-08-09 20:49:29 +0900

    수업_0809

---
+++ Assets/03.Scripts/turretRotate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretRotate : MonoBehaviour
+{
+    public float turnSpeed = 840f;
+    // Use this for initialization
+    void Start()
+    {
+        a = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Rotate_X = " + transform.rotation.x);
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0f, -1f * turnSpeed * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+            transform.Rotate(turnSpeed * Time.deltaTime, 0f, 0f);
+        if (Input.GetKey(KeyCode.LeftShift))
+            transform.Rotate(-1f * turnSpeed * Time.deltaTime, 0f, 0f);
+
+
+    }
+}
