671dc5eb478bd1cd94b1ba5eb395cfecf6b3d792
Mellow1213 <dpcks7282@gmail.com>
2021-08-11 20:39:13 +0900

    수업_0811
    1. 구글 카드보드 기반 VR 영상 볼 수 있는 기능 -> Scene 0810

---
+++ Assets/03. Scripts/0811/Place.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Place : MonoBehaviour
+{
+    public Material placeMaterial;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
