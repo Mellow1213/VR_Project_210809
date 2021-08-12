c931f8421c1cd49c2486e9c668ca24ffffc4253d
Mellow1213 <dpcks7282@gmail.com>
2021-08-12 21:00:23 +0900

    0812수업

---
+++ Assets/03. Resources/Scripts/Picture.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picture : MonoBehaviour
+{
+    public GameObject viewInfo;
+    public GameObject viewVideo;
+    private int page = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitView();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void InitView()
+    {
+        viewInfo.SetActive(false);
+        viewVideo.SetActive(false);
+        page = 0;
+    }
+
+
+
+    public void ViewInformation()
+    {
+        if(page == 0)
+        {
+            viewInfo.SetActive(true);
+        }
+        else if(page == 1)
+        {
+            viewInfo.SetActive(false);
+            viewVideo.SetActive(true);
+
+        }
+        else if (page == 2)
+        {
+            viewVideo.SetActive(false);
+            page = -1;
+
+        }
+
+        page++;
+    }
+}
---
+++ Assets/03. Resources/Scripts/PlayerMove.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 3;
+
+    public Text textMessage;
+
+    private bool isViewPicture = false;
+    private bool isMove = false;
+
+
+    private Action initView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMessage.text = "Touch to Move";
+        textMessage.color = Color.blue;
+
+
+
+        Picture[] pictures = FindObjectsOfType<Picture>();
+        foreach(Picture p in pictures)
+        {
+            initView += p.InitView;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit; 
+        Physics.Raycast(player.transform.position, player.forward, out hit, 10);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isViewPicture)
+            {
+                Picture picture = null;
+
+                if (hit.collider != null)
+                    picture = hit.collider.GetComponent<Picture>();
+
+                if (picture != null)
+                {
+                    isMove = false;
+                    picture.ViewInformation();
+                }
+                else
+                {
+                    isMove = true;
+                }
+            }
+            else
+            {
+                isMove = true;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+
+            isMove = false;
+        }
+
+
+        if (isMove)
+        {
+
+            Vector3 dir = player.forward;
+            dir.y = 0;
+            this.transform.position += dir * Time.deltaTime * speed;
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Trigger"))
+        {
+            isViewPicture = true;
+            textMessage.text = "Touch Picture";
+            textMessage.color = Color.red;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Trigger"))
+        {
+            isViewPicture = false;
+            textMessage.text = "Touch to Move";
+            textMessage.color = Color.blue;
+        }
+
+
+        initView?.Invoke();
+    }
+}
