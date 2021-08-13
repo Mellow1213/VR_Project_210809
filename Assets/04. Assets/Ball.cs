3cf058b01ffa51781ab1bf7d25559667b7bd9763
Mellow1213 <dpcks7282@gmail.com>
2021-08-13 20:46:03 +0900

    수업0813

---
+++ Assets/04. Assets/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn.transform.position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Distance = " + Vector3.Distance(this.transform.position, plane.transform.position));
+        if (Vector3.Distance(this.transform.position, plane.transform.position) > 80) 
+            this.transform.position = spawn.transform.position;
+    }
+}
